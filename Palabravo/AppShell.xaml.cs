dcad7c9f40829af1b8bd678cc80701b41f542a8e
Ricardo R <rretamal.dev@gmail.com>
2026-09-03 20:07:32 -0400

    Initial files

---
+++ Palabravo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Palabravo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Palabravo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Palabravo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
